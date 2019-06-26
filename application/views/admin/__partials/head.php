099a747fdd7494da14ba39c1e7c5d81a48cb2c32
M Thariq Nugroho <thoriqnugroho3@gmail.com>
2019-06-26 16:41:13 +0700

    Update head.php

--- application/views/admin/__partials/head.php
+++ application/views/admin/__partials/head.php
@@ -5,7 +5,7 @@
 <meta name="description" content="">
 <meta name="author" content="">
 <!-- Favicon icon -->
-<link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('assets/images/favicon.png'); ?>">
 <title><?php echo SITE_NAME; ?></title>
 <!-- Bootstrap Core CSS -->
 <link href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
